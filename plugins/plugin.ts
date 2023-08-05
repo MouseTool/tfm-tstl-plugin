469bde5046608555a2338951bc88562fab688ea4
Cassolette <crollierollz@gmail.com>
2023-08-05 14:14:57 +0800

    move polyfill to Printer to maintain sourcemap offsets

--- plugins/plugin.ts
+++ plugins/plugin.ts
@@ -1,86 +1,48 @@
 import ts from "typescript";
 import * as tstl from "typescript-to-lua";
-import { CompilerOptions, isBundleEnabled } from "typescript-to-lua";
-import { transformLuaLibFunction } from "typescript-to-lua/dist/transformation/utils/lualib";
 import { TfmCustomPrinter } from "./printer";
-import { basename } from "./utils";
 
 const entrypointRegex = /return (require\("[\w.]+", \.{3}\))\s*$/;
+
 const plugin: tstl.Plugin = {
+  beforeTransform(
+    program: ts.Program,
+    options: tstl.CompilerOptions,
+    emitHost: tstl.EmitHost
+  ) {
+    if (!tstl.isBundleEnabled(options))
+      return [
+        {
+          messageText:
+            "Using tfm-tstl-plugin without setting `luaBundle` is no-op.",
+          category: ts.DiagnosticCategory.Warning,
+          code: 0,
+          source: "tsconfig.json",
+          file: undefined,
+          length: undefined,
+          start: undefined,
+        },
+      ];
+  },
+
   beforeEmit(
     program: ts.Program,
     options: tstl.CompilerOptions,
     emitHost: tstl.EmitHost,
     result: tstl.EmitFile[]
   ) {
-    //const diag : ts.Diagnostic[] = []
-    if (!isBundleEnabled(options)) return;
+    if (!tstl.isBundleEnabled(options)) return;
 
     const bundle = result[0];
     console.assert(bundle != null);
 
-    let prependCode = `--[[\n    ${basename(
-      bundle.outputPath
-    )}\n    Generated on ${new Date().toUTCString()}\n    https://github.com/TypeScriptToLua/TypeScriptToLua\n]]--\n`;
-
-    prependCode += `_VERSION = "Lua 5.2"\n`;
-
-    prependCode += `do
-    local raw_print, raw_xpcall = print, xpcall
-    local oTableConcat, oUnpack = table.concat, table.unpack
-    _G.__TS__originalPrint = raw_print
-    _G.__TS__originalXpcall = raw_xpcall
-    print = function(...)
-        local args = {...}
-        local nargs = select('#', ...)
-        local segments = {}
-        for i = 1, nargs do
-            segments[i] = tostring(args[i])
-        end
-        return raw_print(oTableConcat(segments, "\t"))
-    end
-    xpcall = function(f, msgh, ...)
-        local args, nargs = {...}, select("#", ...)
-        return raw_xpcall(function()
-            return f(oUnpack(args, 1, nargs))
-        end, msgh)
-    end
-end
-`;
-
-    prependCode += `do
-    local raw_print, raw_xpcall = print, xpcall
-    local oTableConcat, oUnpack = table.concat, table.unpack
-    _G.__TS__originalPrint = raw_print
-    _G.__TS__originalXpcall = raw_xpcall
-    print = function(...)
-        local args = {...}
-        local nargs = select('#', ...)
-        local segments = {}
-        for i = 1, nargs do
-            segments[i] = tostring(args[i])
-        end
-        return raw_print(oTableConcat(segments, "\t"))
-    end
-    xpcall = function(f, msgh, ...)
-        local args, nargs = {...}, select("#", ...)
-        return raw_xpcall(function()
-            return f(oUnpack(args, 1, nargs))
-        end, msgh)
-    end
-end
-`;
-
     if (options.sourceMapTraceback) {
-      prependCode += `do
-    debug.getinfo = function(level)
-        return { short_src = string.match(debug.traceback(nil, 1), "(%S+%.lua):%d+") }
-    end
-end
-`;
+      // Replace unsupported debug.getinfo(1) with one-liner alterntive
+      bundle.code = bundle.code.replace(
+        `debug.getinfo(1).short_src`,
+        `string.match(debug.traceback(nil, 1), "(%S+%.lua):%d+")`
+      );
     }
-
-    bundle.code = prependCode + bundle.code;
 
     const entrypointMatch = bundle.code.match(entrypointRegex);
     if (!entrypointMatch) {
@@ -88,7 +50,9 @@ end
     }
     const [_, entrypointRequire] = entrypointMatch;
 
-    bundle.code = bundle.code.replace(entrypointRegex, entrypointRequire) + "\n";
+    // Remove unsupported top level return
+    bundle.code =
+      bundle.code.replace(entrypointRegex, entrypointRequire) + "\n";
   },
 
   printer: (
@@ -97,12 +61,6 @@ end
     fileName: string,
     file: tstl.File
   ) => new TfmCustomPrinter(emitHost, program, fileName).print(file),
-
-  /*visitors: {
-    [ts.SyntaxKind.AwaitExpression]: (node, context) => {
-      return transformLuaLibFunction()
-    }
-  }*/
 };
 
 export default plugin;
