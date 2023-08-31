201a477538accb952692472de6c7ddf9f565f94c
Cassolette <crollierollz@gmail.com>
2023-08-31 23:45:20 +0800

    delete build output

--- src/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.basename = void 0;
-const basename = (sPath) => sPath.split(/[\\/]/).pop();
-exports.basename = basename;
